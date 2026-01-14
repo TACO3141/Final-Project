fb2c73b8626f0ce21979fc921da87967f0dac33d
TACO3141 <andrewej10@gmail.com>
2026-01-14 11:35:48 -0700

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -1,4 +1,16 @@
 // keeping this here: 🟩🟨⬛
+import java.util.Scanner;
+import java.io.file;
+
 public class Wordle
 {
+  private final String WORD;
+
+  public Wordle()
+  {
+    Scanner wordleScanner = new Scanner(wordList.txt);
+    int lineNum = Math.round((Math.random() * 14854) + 1);
+    wordleScanner.useDelimiter("\n");
+    for (i = 0; i < lineNum; i++)
+  }
 }
