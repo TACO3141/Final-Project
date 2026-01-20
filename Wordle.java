017df05fc5492cd2da8f537e7fd1f5e792b29901
TACO3141 <andrewej10@gmail.com>
2026-01-19 21:53:12 -0700

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -7,11 +7,11 @@ import java.time.Instant;
 public class Wordle
 {
   // final so I dont accidentally change word after it's set
-  private final String WORD;
+  private static String word;
   private int guesses;
   private Instant time;
   
-  /* Todo: create a file after asking for pathname (If it doesnt exist already) and then store info.
+  /* Todo: create a file and folder after taking pathname and then store info.
   * If the file exists, say welcome back and check if time is over 24 hours since last game.
   
   * Lets do streaks too, where the program will check if its been over 24 hours since last game, 
@@ -24,39 +24,34 @@ public class Wordle
   */
   public Wordle()
   {
-    // I'm just trying this, lets see if it works or if it gives me some problems
-    // (in which I would need to go input the full pathname after asking the user for it).
-    File info = new File("wordleStats.txt");
-    boolean wordleStatsExists = info.createNewFile("wordleStats.txt");
-    if (wordleStatsExists)
+    File folder = new file("C:\\Users\\" + System.getProperty("user.name") + "\\Wordle");
+    File info = new File("wordleStats.txt"); // Create a file object for stats document
+    boolean folderExists;
+    boolean infoExists;
+    try
     {
-      // go through the file to check the time and streak.
+      folderExists = 
     }
     
     // Save the time, we'll store it in a file soon
     time = Instant.now();
-    // take the scanner to scan the wordlist file, go down to the line you want (random) and then take that word.
-    try:
-    {
-      File wordList = new File("wordList.txt");
-      Scanner wordleScanner = new Scanner(wordList.txt);
-      int lineNum = Math.round((Math.random() * 14854) + 1);
-      wordleScanner.useDelimiter("\n");
-      for (i = 0; i < lineNum; i++)
-      {
-        wordleScanner.next();
-      }
-      WORD = wordleScanner.next();
+    
+    int lineNum = (int) Math.round((Math.random() * 14854) + 1.5);
+		try
+		{
+			File wordList = new File("wordList.txt"); 
+			BufferedReader wordFinder = new BufferedReader(new FileReader("wordList.txt"));
+			for (int i = 0; i < lineNum - 1; i++)
+			{
+				word = wordFinder.readLine();
+			}
+			word = wordFinder.readLine();
     }
-   catch (FileNotFoundException e)
-    {
-      System.out.println("Sorry, the wordlist was not found. try reinstalling the program and making sure the wordlist is in the same directory (folder) as the game.")
-    }
-    // JUST FOR TESTING! REMOVE THIS LINE AFTER!!
-    System.out.println(lineNum + " " + WORD);
-    // JUST FOR TESTING! REMOVE THIS LINE AFTER!!
-    // Time storage!
-  }
+		catch (Exception e)
+		{
+      System.out.println("Sorry, an error occured. Please try running the program again. If that doesn't work, reinstall the program.");
+		}
+
 
   public String guess()
   {
